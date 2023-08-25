acd4e919f70668eae7bef3ac1efdfa7c1520cc4d
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-08-25 16:42:03 -0400

    transcription service working on the backend

--- audio_service/audio_app/views.py
+++ audio_service/audio_app/views.py
@@ -2,24 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt 
 import json 
-import base64
+import requests
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world. You're at the polls index")
 
+
+def generate_important_fields(text):
+    pass
+
+
 @csrf_exempt
 def transcribe_audio(request):
-    print("Executed")
     if request.method == 'POST':
         # Parse the JSON data from the request body
         data = json.loads(request.body.decode('utf-8'))
-
-        # Extract information from the data
         audio_byte_string = data.get('audio_byte_string')
-        print("Here's your audio data", audio_byte_string)
-        audio_data = base64.b64decode(audio_byte_string)
-        with open('testing123.mp3', 'wb') as audio_file:
-            audio_file.write(audio_data)
+        #send the audio_byte_string to something on the google cloud for transcritptions
+
+        payload = {'audio_byte_string':audio_byte_string}
+        url = 'http://127.0.0.1:5000/transcription'
+        response = requests.post(url=url, json=payload)
+        print(response)
 
         return JsonResponse({'status': 'success'})
 
